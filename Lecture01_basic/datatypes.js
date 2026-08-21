c8d5844ba04a4b6d3ad5f33f60845686e848ba15
Ayush1216-maker <ayushkohli1216@gmail.com>
2026-08-21 14:11:15 +0000

    datetype

---
+++ Lecture01_basic/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert("hello")// we are using nodejs not browser
+let name="king"
+let age=32
+let isLoggedIn=false;
+
+//number ==>2^53
+//bigint
+//string==>""perfer ,'' avoid
+//boolean
+//null==>standalone value
+//undefined==>when value is not defined
+//symbol used to find uniqueness specially in react
+
+//object
+console.log(typeof(age));
+console.log(typeof(null));//oject 
+console.log(typeof(undefined));//undefined
+
+
