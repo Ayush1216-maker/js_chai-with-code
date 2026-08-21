055b7aa4428d770e6aff80f168602e224780218b
Ayush1216-maker <ayushkohli1216@gmail.com>
2026-08-21 14:21:39 +0000

    converion

---
+++ Lecture01_basic/datetype_conversion_confusio.js
@@ -0,0 +1,16 @@
+// let score="3abd"
+let score=null
+// let score="33"
+console.log(typeof score);
+console.log(typeof(score));
+let valueNumber=Number(score);
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+// "33"-->33
+//"33avw"-->NaN
+//true-->1
+//null-->0
+let isLoggedIn=1
+let booleanIdLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIdLoggedIn);
